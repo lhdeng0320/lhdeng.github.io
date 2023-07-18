3dccdfc12a0cb1a6f4be9a116f8647699b3e278f
lhdeng320 <87566781+lhdeng320@users.noreply.github.com>
2023-07-18 12:00:29 +0800

    Update switch.js

--- js/switch.js
+++ js/switch.js
@@ -2,5 +2,5 @@ var i = 1;
 setInterval(function(){
         i++;
         if(i>4){i=1;}
-        document.getElementById('pic').src="image/"+i+".jpg";
+        document.getElementById('pic').src="images/"+i+".jpg";
 },2000);
